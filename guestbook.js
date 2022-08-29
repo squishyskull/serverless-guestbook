c95df233ee05ff8df621881d3ad44df3ad9428bc
squishyskull <tamara.e.brooks1@gmail.com>
2022-08-29 20:14:53 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://6b6fd44d.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
